88e8dd214d61d54a3a438958f0c345abe8359470
Cesar Garcia Pont <cesar.garcia@hasbro.es>
2016-09-15 16:52:55 +0200

    First functional version

--- module1_solution/app.js
+++ module1_solution/app.js
@@ -6,11 +6,24 @@ angular.module('LunchCheck', [])
 
 LunchCheckController.$inject = ['$scope'];
 function LunchCheckController($scope) {
-  $scope.numberOfItems = 0;
+  var numberOfItems = 0;
+  $scope.messageDisplayedToUser = "";
 
-  // $scope.checkNumberOfItems = function() {
-  //   $scope.numberOfItems = 666;
-  // };
+  $scope.checkIfTooMuch = function() {
+    if ($scope.lunchMenuText != undefined && $scope.lunchMenuText != ''){
+      var arrayOfItems = $scope.lunchMenuText.split(',');
+      numberOfItems = arrayOfItems.length;
+    } else {
+      numberOfItems = 0;
+    }
+    if (numberOfItems > 0 && numberOfItems < 4) {
+        $scope.messageDisplayedToUser = "Enjoy!";
+    } else if (numberOfItems > 3) {
+        $scope.messageDisplayedToUser = "Too much!";
+    } else {
+        $scope.messageDisplayedToUser = "Please enter data first";
+    }    
+  };
 
 }
 
