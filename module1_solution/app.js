ac65ef18920c40f6ddfdb5e7eecebe60c32faa71
Cesar Garcia Pont <cesar.garcia@hasbro.es>
2016-09-15 17:19:14 +0200

    Refactoring - created function getNumberOfItems

--- module1_solution/app.js
+++ module1_solution/app.js
@@ -10,20 +10,25 @@ function LunchCheckController($scope) {
   $scope.messageDisplayedToUser = "";
 
   $scope.checkIfTooMuch = function() {
-    if ($scope.lunchMenuText != undefined && $scope.lunchMenuText != ''){
-      var arrayOfItems = $scope.lunchMenuText.split(',');
-      numberOfItems = arrayOfItems.length;
-    } else {
-      numberOfItems = 0;
-    }
+    numberOfItems = getNumberOfItems($scope.lunchMenuText);
     if (numberOfItems > 0 && numberOfItems < 4) {
         $scope.messageDisplayedToUser = "Enjoy!";
     } else if (numberOfItems > 3) {
         $scope.messageDisplayedToUser = "Too much!";
     } else {
         $scope.messageDisplayedToUser = "Please enter data first";
-    }    
+    }
   };
+
+  function getNumberOfItems( text ){    
+    if (text != undefined && text != ''){
+      var arrayOfItems = text.split(',');
+      numberOfItems = arrayOfItems.length;
+    } else {
+      numberOfItems = 0;
+    }
+    return numberOfItems;
+  }
 
 }
 
