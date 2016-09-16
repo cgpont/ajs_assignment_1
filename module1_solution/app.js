ed390066e9a3b30c2d9989378e5c86c1a1f753fb
Cesar Garcia Pont <cesar.garcia@hasbro.es>
2016-09-16 09:35:34 +0200

    Added logic to ignore empty items in the count

--- module1_solution/app.js
+++ module1_solution/app.js
@@ -11,6 +11,7 @@ function LunchCheckController($scope) {
   $scope.messageDisplayedToUserClass = "";
 
   $scope.checkIfTooMuch = function() {
+    //Please note that any empty item (i.e., , ,) won't be considered as an item towards to the count.
     numberOfItems = getNumberOfItems($scope.lunchMenuText);
     if (numberOfItems > 0 && numberOfItems < 4) {
         $scope.messageDisplayedToUser = "Enjoy!";
@@ -27,12 +28,25 @@ function LunchCheckController($scope) {
   function getNumberOfItems( text ){
     if (text != undefined && text != ''){
       var arrayOfItems = text.split(',');
+      //Please note that any empty item (i.e., , ,) won't be considered as an item towards to the count.
+      arrayOfItems.clean(undefined);
+      arrayOfItems.clean("");
       numberOfItems = arrayOfItems.length;
     } else {
       numberOfItems = 0;
     }
     return numberOfItems;
   }
+
+  Array.prototype.clean = function(deleteValue) {
+    for (var i = 0; i < this.length; i++) {
+      if (this[i].trim() == deleteValue) {
+        this.splice(i, 1);
+        i--;
+      }
+    }
+    return this;
+  };
 
 }
 
