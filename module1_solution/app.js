a935060ca629d9b82030dc0a116f7c9dc1552c1a
Cesar Garcia Pont <cesar.garcia@hasbro.es>
2016-09-16 09:07:15 +0200

    Added color and border styles to different messages displayed to user

--- module1_solution/app.js
+++ module1_solution/app.js
@@ -8,19 +8,23 @@ LunchCheckController.$inject = ['$scope'];
 function LunchCheckController($scope) {
   var numberOfItems = 0;
   $scope.messageDisplayedToUser = "";
+  $scope.messageDisplayedToUserClass = "";
 
   $scope.checkIfTooMuch = function() {
     numberOfItems = getNumberOfItems($scope.lunchMenuText);
     if (numberOfItems > 0 && numberOfItems < 4) {
         $scope.messageDisplayedToUser = "Enjoy!";
+        $scope.messageDisplayedToUserClass = "enjoy";
     } else if (numberOfItems > 3) {
         $scope.messageDisplayedToUser = "Too much!";
+        $scope.messageDisplayedToUserClass = "too-much";
     } else {
         $scope.messageDisplayedToUser = "Please enter data first";
+        $scope.messageDisplayedToUserClass = "enter-data";
     }
   };
 
-  function getNumberOfItems( text ){    
+  function getNumberOfItems( text ){
     if (text != undefined && text != ''){
       var arrayOfItems = text.split(',');
       numberOfItems = arrayOfItems.length;
